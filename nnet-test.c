cdd7f4976aa11d1f5bf43129fdb827d96a98f31a
J. David Lowe <j.david.lowe@gmail.com>
2011-12-29 15:55:36 -0800

    test 0 and \xFF bytes

--- nnet-test.c
+++ nnet-test.c
@@ -8,7 +8,7 @@ extern void bigrammer(FILE *f);
 
 #define IDX(s) ((s[0]) * 256 + (s[1]))
 
-void bigram_string(char *s, int n, ...) {
+void bigram_string(char *s, int len, int n, ...) {
     char template[256] = "/tmp/tfile.XXXXXX";
     char *filename = mktemp(template);
     FILE *f = fopen(filename, "w+");
@@ -16,7 +16,7 @@ void bigram_string(char *s, int n, ...) {
     float expected_bigrams[256 * 256];
     va_list ap;
 
-    fprintf(f, "%s", s);
+    write(fileno(f), s, len);
     fflush(f);
     fseek(f, 0, SEEK_SET);
 
@@ -31,8 +31,9 @@ void bigram_string(char *s, int n, ...) {
     }
     va_start(ap, n);
     for (i = 0; i < n; ++i) {
-        char *bigram = va_arg(ap, char *);
+        unsigned char *bigram = va_arg(ap, unsigned char *);
         double x = va_arg(ap, double);
+
         expected_bigrams[IDX(bigram)] = x;
     }
     va_end(ap);
@@ -40,8 +41,8 @@ void bigram_string(char *s, int n, ...) {
     /* assert expectations */
     for (i = 0; i < (256 * 256); ++i) {
         fail_unless((bigrams[i] == expected_bigrams[i]),
-            "bigramming '%s': bigram '%c%c' expected %f, actual %f",
-            s, i / 256, i % 256, expected_bigrams[i], bigrams[i]);
+            "bigramming '%s': bigram '%c%c' (%d.%d) expected %f, actual %f",
+            s, i / 256, i % 256, i / 256, i % 256, expected_bigrams[i], bigrams[i]);
     }
     return;
 }
@@ -49,15 +50,19 @@ void bigram_string(char *s, int n, ...) {
 START_TEST (test_bigrammer)
 {
     /* boundary: need at least 2 bytes to get a bigram :) */
-    bigram_string("", 0);
-    bigram_string("a", 0);
+    bigram_string("", 0, 0);
+    bigram_string("a", 1, 0);
 
     /* basic maths */
-    bigram_string("AA", 1, "AA", 1.0F);
-    bigram_string("asdf", 3, "as", 1.0F / 3.0F, "sd", 1.0F / 3.0F, "df", 1.0F / 3.0F);
-    bigram_string("aaab", 2, "aa", 2.0F / 3.0F, "ab", 1.0F / 3.0F);
-    bigram_string("aaaa", 1, "aa", 1.0F);
-    bigram_string("aabb", 3, "aa", 1.0F / 3.0F, "ab", 1.0F / 3.0F, "bb", 1.0F / 3.0F);
+    bigram_string("AA",   2, 1, "AA", 1.0F);
+    bigram_string("asdf", 4, 3, "as", 1.0F / 3.0F, "sd", 1.0F / 3.0F, "df", 1.0F / 3.0F);
+    bigram_string("aaab", 4, 2, "aa", 2.0F / 3.0F, "ab", 1.0F / 3.0F);
+    bigram_string("aaaa", 4, 1, "aa", 1.0F);
+    bigram_string("aabb", 4, 3, "aa", 1.0F / 3.0F, "ab", 1.0F / 3.0F, "bb", 1.0F / 3.0F);
+
+    /* byte boundaries... */
+    bigram_string("\0\0", 2, 1, "\0\0", 1.0F);
+    bigram_string("\xFF\xFF", 2, 1, "\xFF\xFF", 1.0F);
 }
 END_TEST
 
