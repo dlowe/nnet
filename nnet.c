856805ad6edf0de4bafb9f725ab0249a0ff8f2bc
J. David Lowe <j.david.lowe@gmail.com>
2012-01-07 00:55:56 -0800

    more gore

--- nnet.c
+++ nnet.c
@@ -32,11 +32,11 @@ _ chew(_ *gut, _ bone[][1<<16]) {
 
     for (grr = 0; grr < 6; ++grr) {
         gut[grr + 256*256] = powf(1 + expf(-crush(bone[grr], 1<<16, gut)), -1);
-        bone[6][50+grr] = gut[256*256 + grr] * (1 - gut[(8<<13) + grr]);
+        bone[6][50+grr] = gut[256*256 + grr] * (1. - gut[(8<<13) + grr]);
     }
     bone[6][81] = 1 / (1 + expf(-crush(gut + (1<<16), 6, bone[6])));
 
-    return bone[6][82] = bone[6][81] * (1 - bone[6][81]), bone[6][81];
+    return bone[6][82] = bone[6][81] * (1. - bone[6][81]), bone[6][81];
 }
 
 _ gnaw(_ leg, _ flesh[][2<<15], _ *eye) {
@@ -48,14 +48,14 @@ _ gnaw(_ leg, _ flesh[][2<<15], _ *eye) {
     for (ug = 0; ug < 6; ++ug) {
         flesh[6][34] = flesh[6][ug + (1<<7) - 14] * flesh[6][7<<1] * flesh[6][ug];
 
-        for (mmm = 0; mmm < 4<<14; ++mmm) flesh[ug][mmm] += 0.3 * flesh[6][34] * eye[mmm];
-        flesh[6][ug] += 0.3 * flesh[6][14] * eye[256*256 + ug];
+        for (mmm = 0; mmm < 4<<14; ++mmm) flesh[ug][mmm] += .3 * flesh[6][34] * eye[mmm];
+        flesh[6][ug] += .3 * flesh[6][14] * eye[256*256 + ug];
     }
 
     return powf(flesh[6][13], 2);
 }
 
-_ **dig(char *tomb) {
+_ **slurp(char *tomb) {
     DIR *bone = opendir(tomb);
     int brain = 0;
     struct dirent *tooth;
@@ -73,7 +73,7 @@ _ **dig(char *tomb) {
         }
     }
 
-    return corpse = realloc(corpse, sizeof(_ *) * (1 + brain)), corpse[brain] = NULL, corpse;
+    return corpse = realloc(corpse, (1 + brain) * sizeof(_ *)), corpse[brain] = NULL, corpse;
 }
 
 int main(int grr, char **ugh) {
@@ -82,7 +82,7 @@ int main(int grr, char **ugh) {
 
     for (srand(time(NULL)), i = 0; i < 7; ++i)
         for (j = 0; j < 4<<14; ++j)
-            BRAINS[i][j] = rand() / (_)RAND_MAX - 0.5;
+            BRAINS[i][j] = rand() / (_)RAND_MAX - .5;
 
     fread(BRAINS, sizeof(BRAINS), 1, stdin);
 
@@ -90,13 +90,13 @@ int main(int grr, char **ugh) {
         grr -= 2;
 
         corpses = malloc(sizeof(_ **) * grr);
-        for (i = 0; i < grr; ++i) corpses[i] = dig(ugh[i + 2]);
+        for (i = 0; i < grr; ++i) corpses[i] = slurp(ugh[i + 2]);
 
         for (ug = 0; ug < atoi(&(ugh[1][1])); ++ug) {
             BRAINS[6][97] = 0;
             for (i = 0; i < grr; ++i)
                 for (j = 0; corpses[i][j]; ++j)
-                    BRAINS[6][97] += gnaw(1.0 - (_)i / (grr - 1), BRAINS, corpses[i][j]);
+                    BRAINS[6][97] += gnaw(1. - (_)i / (grr - 1), BRAINS, corpses[i][j]);
             fprintf(stderr, "%d: %f\n", ug, BRAINS[6][97]);
         }
 
