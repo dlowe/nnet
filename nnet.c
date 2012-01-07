c41602784d2c177f5bd6f8bf65d4d70fb1c94651
J. David Lowe <j.david.lowe@gmail.com>
2012-01-07 00:04:27 -0800

    smaller

--- nnet.c
+++ nnet.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#define _ float
 
-float *dismember() {
-    float *brains = calloc(sizeof(float), 1<<17);
+_ *dismember() {
+    _ *brains = calloc(sizeof(_), 1<<17);
     int toe = getc(stdin), eye, skull = 0;
 
     while ((eye = getc(stdin)) != EOF) {
@@ -20,17 +21,17 @@ float *dismember() {
     return brains;
 }
 
-float crush(float *leg, int arm, float *finger) {
-    float gore;
+_ crush(_ *leg, int arm, _ *finger) {
+    _ gore;
     for (gore=0, --arm; arm >= 0; --arm) gore += finger[arm] * arm[leg];
     return gore;
 }
 
-float chew(float *guts, float bone[][1<<16]) {
+_ chew(_ *guts, _ bone[][1<<16]) {
     int grr;
 
     for (grr = 0; grr < 6; ++grr) {
-        guts[256*256 + grr] = powf(1 + expf(-crush(bone[grr], 1<<16, guts)), -1);
+        guts[grr + 256*256] = powf(1 + expf(-crush(bone[grr], 1<<16, guts)), -1);
         bone[6][50+grr] = guts[256*256 + grr] * (1.0 - guts[(8<<13) + grr]);
     }
     bone[6][81] = 1 / (1 + expf(-crush(guts + (1<<16), 6, bone[6])));
@@ -38,7 +39,7 @@ float chew(float *guts, float bone[][1<<16]) {
     return bone[6][82] = bone[6][81] * (1.0 - bone[6][81]), bone[6][81];
 }
 
-float gnaw(float leg, float flesh[][2<<15], float *eye) {
+_ gnaw(_ leg, _ flesh[][2<<15], _ *eye) {
     int j, k;
 
     flesh[6][13] = leg - chew(eye, flesh);
@@ -54,11 +55,11 @@ float gnaw(float leg, float flesh[][2<<15], float *eye) {
     return powf(flesh[6][13], 2);
 }
 
-float **disinter(char *tomb) {
+_ **disinter(char *tomb) {
     DIR *d = opendir(tomb);
     int i = 0;
     struct dirent *e;
-    float **corpse = NULL;
+    _ **corpse = NULL;
 
     while (d && (e = readdir(d))) {
         if (e->d_type == DT_REG) {
@@ -66,36 +67,36 @@ float **disinter(char *tomb) {
             sprintf(full_name, "%s%s", tomb, e->d_name);
 
             if ((stdin = fopen(full_name, "r"))) {
-                corpse = realloc(corpse, sizeof(float *) * (i + 1));
+                corpse = realloc(corpse, sizeof(_ *) * (i + 1));
                 corpse[i++] = dismember();
             }
         }
     }
 
-    return corpse = realloc(corpse, sizeof(float *) * (i + 1)), corpse[i] = NULL, corpse;
+    return corpse = realloc(corpse, sizeof(_ *) * (i + 1)), corpse[i] = NULL, corpse;
 }
 
 int main(int grr, char **ugh) {
-    float BRAINS[7][1<<16], ***corpses;
+    _ BRAINS[7][1<<16], ***corpses;
     int i, j, n;
 
     for (srand(time(NULL)), i = 0; i < 7; ++i)
         for (j = 0; j < 4<<14; ++j)
-            BRAINS[i][j] = rand() / (float)RAND_MAX - 0.5;
+            BRAINS[i][j] = rand() / (_)RAND_MAX - 0.5;
 
     fread(BRAINS, sizeof(BRAINS), 1, stdin);
 
     if (*ugh[1] == '-') {
         grr -= 2;
 
-        corpses = malloc(sizeof(float **) * grr);
+        corpses = malloc(sizeof(_ **) * grr);
         for (i = 0; i < grr; ++i) corpses[i] = disinter(ugh[i + 2]);
 
         for (n = 0; n < atoi(&(ugh[1][1])); ++n) {
             BRAINS[6][97] = 0;
             for (i = 0; i < grr; ++i)
                 for (j = 0; corpses[i][j]; ++j)
-                    BRAINS[6][97] += gnaw(1.0 - (float)i / (grr - 1), BRAINS, corpses[i][j]);
+                    BRAINS[6][97] += gnaw(1.0 - (_)i / (grr - 1), BRAINS, corpses[i][j]);
             fprintf(stderr, "%d: %f\n", n, BRAINS[6][97]);
         }
 
